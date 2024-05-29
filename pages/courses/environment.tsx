5a28ed0216095c88705a5140f687f2de838c6e8d
Rishabh <chaudharyrishabh029@gmail.com>
2024-05-29 09:55:53 +0530

    added default export to pages

---
+++ pages/courses/environment.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Environment = () => {
+    return (
+        <div>environment</div>
+    )
+}
+
+export default Environment
---
+++ pages/courses/machine-learning.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MachineLearning = () => {
+    return (
+        <div>machine-learning</div>
+    )
+}
+
+export default MachineLearning
