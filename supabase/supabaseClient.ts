6eae749f4844e79e7e03b97babd213d485006be9
Rishabh <chaudharyrishabh029@gmail.com>
2024-07-21 13:37:44 +0530

    changed names to next_public supabaseclient

--- supabase/supabaseClient.ts
+++ supabase/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-console.log("supdatabase_key:-",process.env.SUPABASE_URL, ", anon_key:-",process.env.SUPABASE_ANON_KEY )
-const supabaseUrl = process.env.SUPABASE_URL_KEY as string;
-const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
+console.log("supdatabase_key:-",process.env.NEXT_PUBLIC_SUPABASE_URL, ", anon_key:-",process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY )
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_KEY as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
