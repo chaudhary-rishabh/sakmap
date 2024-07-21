e260fd8845a50014b48a08752dd3092027dc28b0
Rishabh <chaudharyrishabh029@gmail.com>
2024-07-21 13:23:37 +0530

    added console.log for debug

--- supabase/supabaseClient.ts
+++ supabase/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
+console.log("supdatabase_key:-",process.env.SUPABASE_URL, ", anon_key:-",process.env.SUPABASE_ANON_KEY )
 const supabaseUrl = process.env.SUPABASE_URL_KEY as string;
 const supabaseKey = process.env.SUPABASE_ANON_KEY as string;
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
