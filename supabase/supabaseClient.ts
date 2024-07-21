2675fbde0021e030a61bdaeb31510b853edb4975
Rishabh <chaudharyrishabh029@gmail.com>
2024-07-21 13:53:28 +0530

    removed clg from supabaseclient.ts

--- supabase/supabaseClient.ts
+++ supabase/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-console.log("supdatabase_key:-",process.env.NEXT_PUBLIC_SUPABASE_URL, ", anon_key:-",process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY )
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_KEY as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
